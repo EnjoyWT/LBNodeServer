464afa2ec199993d53e3cfa37d28f45008920927
wtz <wtz@sh.com>
2024-04-10 18:01:46 +0800

    fix： 后台修改为 room 模式

--- server copy.js
+++ server copy.js
@@ -33,34 +33,27 @@ const io = socketIo(server,{
 
 // 用于存储连接的标识符和对应的 socket 实例的映射关系
 const socketMap = new Map();
-const  ROOM_NAME = "roomA"
+
 io.on('connection', (socket) => {
   logger.log('a user connected');
 
     // 处理客户端发送的标识符，并将其存储在映射中
-    socket.join(ROOM_NAME)
-
+    
     socket.on('setIdentifier', (identifier) => {
       logger.log(`${identifier} 已上线`);
-      // // 检查是否已经存在相同的键
-      // if (socketMap.has(identifier)) {
-      //   console.log(`Replacing existing entry for identifier ${identifier}`);
-      //   socketMap.delete(identifier);
-      // }
-      // // 设置新的键值对
-      // socketMap.set(identifier, socket);
+      // 检查是否已经存在相同的键
+      if (socketMap.has(identifier)) {
+        console.log(`Replacing existing entry for identifier ${identifier}`);
+        socketMap.delete(identifier);
+      }
+      // 设置新的键值对
+      socketMap.set(identifier, socket);
 
-      // const jsonData = jsonStorage.readData();
-      // logger.log(` ${identifier}登录时 ,返回给登录者的数据是:  ${JSON.stringify(jsonData)}`);
-      // socket.emit('serverUpdateData', jsonData);
+      const jsonData = jsonStorage.readData();
+      logger.log(` ${identifier}登录时 ,返回给登录者的数据是:  ${JSON.stringify(jsonData)}`);
+      socket.emit('serverUpdateData', jsonData);
 
     });
-
-
-  const jsonData = jsonStorage.readData();
-  logger.log(`登录时 返回 数据是 \n ${JSON.stringify(jsonData)}`);
-  socket.emit('serverUpdateData', jsonData);
-
   // 处理客户端断开连接事件，并从映射中移除对应的标识符和 socket 实例
   socket.on('disconnect', () => {
     socketMap.forEach((value, key) => {
@@ -73,30 +66,46 @@ io.on('connection', (socket) => {
 
   // 处理来自客户端的查询数据请求
   socket.on('queryData', (identifier) => {
+
     logger.log(`${identifier} 开始queryData 数据`);
     // 读取JSON文件中的数据
     const jsonData = jsonStorage.readData();
     logger.log(`queryData 返回给 ${identifier}  数据是 \n ${JSON.stringify(jsonData)}`);
-    socket.emit('serverUpdateData', jsonData);
+
+    sendDataToClient(identifier,data)
   });
 
   socket.on('updateData', ({identifier,data}) => {
     logger.log(`${identifier} 开始 updateData 数据 ${data}`);
     // 读取JSON文件中的数据
     jsonStorage.saveData(data);
+    sendDataToClient("H5Admin",data)
   });
 
   // 处理保存数据请求
-  socket.on('saveData', ({ identifier, data }) => {
+  socket.on('h5SaveData', ({ identifier, data }) => {
     logger.log(`收到 ${identifier} : ${JSON.stringify(data)}`);
     
-    jsonStorage.saveData(data);
-    socket.to(ROOM_NAME).emit('serverUpdateData', jsonData);
-  });
+    // 在此处理保存数据的逻辑，例如保存数据到数据库等
 
-  
+    // 反馈给客户端数据已成功保存
+    jsonStorage.saveData(data);
+
+    sendDataToClient("AndroidId",data)
+  });
 });
 
+
+// 函数用于向指定标识符的客户端发送数据
+function sendDataToClient(identifier, data) {
+    const socket = socketMap.get(identifier);
+    if (socket) {
+      socket.emit('serverUpdateData', data);
+      logger.log(`Sent data to client with identifier ${identifier}`);
+    } else {
+      logger.log(`${identifier} , 没在线`);
+    }
+  }
  
   
 const PORT = process.env.PORT || 3000;
