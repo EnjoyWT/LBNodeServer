2c8526e5318aeaeb6a8265709b759b6cb27b0d11
wtz <wtz@sh.com>
2024-04-10 11:23:09 +0800

    初版

--- server.js
+++ server.js
@@ -37,13 +37,27 @@ const socketMap = new Map();
 io.on('connection', (socket) => {
   logger.log('a user connected');
 
+    // 处理客户端发送的标识符，并将其存储在映射中
+    socket.on('setIdentifier', (identifier) => {
+      logger.log(`${identifier} 已上线`);
+      // 检查是否已经存在相同的键
+      if (socketMap.has(identifier)) {
+        console.log(`Replacing existing entry for identifier ${identifier}`);
+        socketMap.delete(identifier);
+      }
+      // 设置新的键值对
+      socketMap.set(identifier, socket);
+
+      const jsonData = jsonStorage.readData();
+      logger.log(` ${identifier}登录时 ,返回给登录者的数据是: \n ${JSON.stringify(jsonData)}`);
+      socket.emit('serverUpdateData', jsonData);
+
+    });
   // 处理客户端断开连接事件，并从映射中移除对应的标识符和 socket 实例
   socket.on('disconnect', () => {
-    logger.log('A client disconnected');
-
     socketMap.forEach((value, key) => {
       if (value === socket) {
-        logger.log(  key + ' client disconnected');
+        logger.log(  key + ' 已下线');
         socketMap.delete(key);
       }
     });
@@ -55,8 +69,9 @@ io.on('connection', (socket) => {
     logger.log(`${identifier} 开始queryData 数据`);
     // 读取JSON文件中的数据
     const jsonData = jsonStorage.readData();
-    logger.log(`返回给 ${identifier}  数据 ${jsonData}`);
-    socket.emit('responseData', jsonData);
+    logger.log(`queryData 返回给 ${identifier}  数据是 \n ${JSON.stringify(jsonData)}`);
+
+    sendDataToClient(identifier,data)
   });
 
   socket.on('updateData', ({identifier,data}) => {
@@ -67,12 +82,14 @@ io.on('connection', (socket) => {
 
   // 处理保存数据请求
   socket.on('h5SaveData', ({ identifier, data }) => {
-    logger.log(`收到 ${identifier} :`, data);
+    logger.log(`收到 ${identifier} : ${JSON.stringify(data)}`);
     
     // 在此处理保存数据的逻辑，例如保存数据到数据库等
 
     // 反馈给客户端数据已成功保存
-    socket.emit('saveConfirmation', 'Data has been saved successfully');
+    jsonStorage.saveData(data);
+
+    sendDataToClient("AndroidId",data)
   });
 });
 
